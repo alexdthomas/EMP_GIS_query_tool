323980824c80907032bc65e8305d86780f82a0cd
alexdthomas <asus4@uw.edu>
2013-08-20 11:00:16 -0600

    first commit

---
+++ EMP_GIS_query_tool.R
@@ -0,0 +1,6 @@
+#Develop tool to query EMP dataset based on global environmental 
+#parameters and identify patterns
+
+#start with example data set and a shiny app
+library(shiny)
+library(phyloseq)
